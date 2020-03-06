f7a853b55b8eb30c18c5b51743d27114b4513c1a
bzli <bzli@LAPTOP-EB56JE7F>
2020-03-06 18:35:58 +0800

    start

---
+++ TaskScheduler-1/src/main/java/com/data/server/TaskScheduler1Application.java
@@ -0,0 +1,13 @@
+package com.data.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduler1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskScheduler1Application.class, args);
+	}
+
+}
